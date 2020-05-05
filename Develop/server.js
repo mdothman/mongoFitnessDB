b98954e5269c32edbd59bcda6743053f96993b40
BEELZ <othmanmuhidean@gmail.com>
2020-05-05 15:08:56 -0700

    pushing to heroku

--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3000
+const logger = require("morgan");
 
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-    useNewUrlParser: true,
-    useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
 });
 
-// routes
-app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+app.listen(PORT, function () {
+  console.log(`Now listening on port: ${PORT}`);
 });
